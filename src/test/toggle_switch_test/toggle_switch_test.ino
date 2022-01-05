7427d19d6b284eba6fa0d50d7cb32c845494ac63
shatakshi0497 <93338170+shatakshi0497@users.noreply.github.com>
2022-01-05 16:55:32 +0100

    Update toggle_switch_test.ino

--- src/test/toggle_switch_test/toggle_switch_test.ino
+++ src/test/toggle_switch_test/toggle_switch_test.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+/* 
+            Toggle Switch Test
 
+  [Lights up internal LED when switch turned on]
+  
+*/
+
+// Pin of toggle switch
+#define SWITCH_PIN 21
+
+// Pin of internal LED of the ESP32
+#define LED_PIN 2
+
+void setup() {
+  pinMode(SWITCH_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(digitalRead(SWITCH_PIN) == HIGH){
+    digitalWrite(LED_PIN, LOW);
+  }
+  if(digitalRead(SWITCH_PIN) == LOW){
+    digitalWrite(LED_PIN, HIGH);
+  }
 }
